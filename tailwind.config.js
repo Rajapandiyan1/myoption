660d84dab316e81926465e1c7cd25b020442cc64
Raja Pandiyan <rajapandiyan1163@gmail.com>
2024-10-02 16:39:07 +0530

    code arr

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 module.exports = {
-  content: [],
+  mode: 'jit',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // Add this line if you're using the App Router (Next.js 13+)
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  // content: [
+  //   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  // ],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // Add this line if you're using the App Router (Next.js 13+)
   ],
   theme: {
     extend: {
