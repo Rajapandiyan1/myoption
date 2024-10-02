1117c88bc0879bd6c549ad91ec5de1026503e848
Raja Pandiyan <rajapandiyan1163@gmail.com>
2024-10-02 18:46:25 +0530

    navbar boostrap

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -32,46 +32,48 @@ const Navbar = ({}) => {
   useEffect(()=>{
     setdashboard(dashpath.path)
       },[dashpath.path])
-  return ( <nav className="bg-gray-800 sticky top-0 z-50">
-    <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-      <div className="flex justify-between items-center h-16">
+  return ( <nav className="bg-dark position-sticky top-0" style={{zIndex:'1020'}}>
+    <div className="container px-2 px-sm-3 px-lg-4">
+      <div className="d-flex justify-content-between align-items-center" style={{height:'64px'}}>
         <div className="flex-shrink-0 text-white">MYOPTION</div>
-        <div className="hidden sm:flex sm:ml-6 space-x-4">
-          <Link href="/" className={`text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${navact.active === 'Home' ? 'bg-white text-gray-900' : 'bg-gray-800 text-gray-300'}`}>
+        <div className="d-none d-sm-flex ms-sm-4 gap-3">
+         
+        
+          <Link href="/" className={`text-secondary hover:bg-dark hover:text-white px-3 py-2 rounded fw-medium ${navact.active === 'Home' ? 'bg-white text-dark' : 'bg-dark text-secondary'}`}>
             Home
           </Link>
 
           {authens.Athen && (
             <Link
-              className={`text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${navact.active === 'Dashboard' ? 'bg-white text-gray-900' : 'bg-gray-800 text-gray-300'}`}
+              className={`text-secondary hover:bg-dark hover:text-white px-3 py-2 rounded fw-medium ${navact.active === 'Home' ? 'bg-white text-dark' : 'bg-dark text-secondary'}`}
               href={`https://myoption-git-master-rajapandiyan1s-projects.vercel.app/${dashboard}`}>
               Dashboard
             </Link>
           )}
 
-          <Link className={`text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${navact.active === 'Register' ? 'bg-white text-gray-900' : 'bg-gray-800 text-gray-300'}`} href="/Register">
+          <Link className={`text-secondary hover:bg-dark hover:text-white px-3 py-2 rounded fw-medium ${navact.active === 'Home' ? 'bg-white text-dark' : 'bg-dark text-secondary'}`} href="/Register">
             Register
           </Link>
 
-          <Link className={`text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${navact.active === 'Login' ? 'bg-white text-gray-900' : 'bg-gray-800 text-gray-300'}`} href="/Login">
+          <Link className={`text-secondary hover:bg-dark hover:text-white px-3 py-2 rounded fw-medium ${navact.active === 'Home' ? 'bg-white text-dark' : 'bg-dark text-secondary'}`} href="/Login">
             Login
           </Link>
         </div>
 
-        <div className="-mr-2 flex sm:hidden">
+        <div className="me-n2 d-flex d-sm-none">
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="text-gray-300 hover:bg-gray-700 hover:text-white inline-flex items-center justify-center p-2 rounded-md"
+            className="text-secondary d-inline-flex align-items-center justify-content-center p-2 rounded custom-hover"
             aria-controls="mobile-menu"
             aria-expanded={isOpen}
           >
             <span className="sr-only">Open main menu</span>
             {isOpen ? (
-              <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <svg className="d-block" style={{height:'1rem',width:'1rem'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
               </svg>
             ) : (
-              <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <svg className="d-block" style={{height:'1rem',width:'1rem'}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
               </svg>
             )}
@@ -80,25 +82,29 @@ const Navbar = ({}) => {
       </div>
     </div>
 
-    <div className={`${isOpen ? 'block' : 'hidden'} sm:hidden`} id="mobile-menu">
-      <div className="px-2 pt-2 pb-3 space-y-1">
-        <Link className={`text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${navact.active === 'Home' ? 'bg-white text-gray-900' : 'bg-gray-800 text-gray-300'}`} href="/">
+    <div className={`${isOpen ? 'd-block' : 'd-none'} d-sm-none`} id="mobile-menu">
+      <div className="px-2 pt-2 pb-3">
+        <Link className={`text-secondary d-block px-3 py-2 rounded fw-medium custom-hover
+  ${navact.active === 'Home' ? 'bg-white text-dark' : 'bg-dark text-secondary'}`} href="/">
           Home
         </Link>
 
         {authens.Athen && (
           <Link
-            className={`text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${navact.active === 'Dashboard' ? 'bg-white text-gray-900' : 'bg-gray-800 text-gray-300'}`}
+            className={`text-secondary d-block px-3 py-2 rounded fw-medium custom-hover
+              ${navact.active === 'Dashboard' ? 'bg-white text-dark' : 'bg-dark text-secondary'}`}
             href={`https://myoption-git-master-rajapandiyan1s-projects.vercel.app/${dashboard}`}>
             Dashboard
           </Link>
         )}
 
-        <Link className={`text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${navact.active === 'Register' ? 'bg-white text-gray-900' : 'bg-gray-800 text-gray-300'}`} href="/Register">
+        <Link className={`text-secondary d-block px-3 py-2 rounded fw-medium custom-hover
+  ${navact.active === 'Register' ? 'bg-white text-dark' : 'bg-dark text-secondary'}`} href="/Register">
           Register
         </Link>
 
-        <Link className={`text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium ${navact.active === 'Login' ? 'bg-white text-gray-900' : 'bg-gray-800 text-gray-300'}`} href="/Login">
+        <Link className={`text-secondary d-block px-3 py-2 rounded fw-medium custom-hover
+  ${navact.active === 'Login' ? 'bg-white text-dark' : 'bg-dark text-secondary'}`} href="/Login">
           Login
         </Link>
       </div>
