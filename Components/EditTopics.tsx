2aa14a155896cdf67d9f52b6a7be28636c83eb2e
Raja Pandiyan <rajapandiyan1163@gmail.com>
2024-10-03 12:38:27 +0530

    w-100

--- Components/EditTopics.tsx
+++ Components/EditTopics.tsx
@@ -278,7 +278,7 @@ function EditTopics({ editId }) {
     <p className="text-black mb-1">Ans:</p>
     <p>{qAndA.answer}</p>
   </div>
-  <div className="mt-3 mt-md-0 d-flex justify-content-end  flex-md-row">
+  <div className="mt-3 w-100 mt-md-0 d-flex justify-content-end  flex-md-row">
     <button
       onClick={() => openEditModal(index)}
       className="btn btn-primary text-white "
