e883772cbd98c49c1e26ab4f2427924d2cc6ca87
Raja Pandiyan <rajapandiyan1163@gmail.com>
2024-10-03 11:36:23 +0530

    edit model-1

--- Components/EditOptions.tsx
+++ Components/EditOptions.tsx
@@ -160,28 +160,29 @@ function Option() {
 
             {/* Modal for delete confirmation */}
             {isModalOpen && (
-  <div className="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-75 z-50">
-    <div className="bg-white rounded-lg shadow-xl w-11/12 sm:w-1/2 lg:w-1/3 p-6">
-      <h2 className="text-xl font-semibold mb-4">Confirm Deletion</h2>
-      <p>
-        Are you sure you want to delete <span className="text-blue-900 font-semibold">{deletetopic}</span> topic?
-      </p>
-      <div className="mt-6 flex justify-end space-x-2">
-        <button 
-          onClick={closeModal} 
-          className="bg-gray-300 text-gray-800 py-2 px-4 rounded-md hover:bg-gray-400 transition duration-200"
-        >
-          Cancel
-        </button>
-        <button 
-          onClick={deleteTopics} 
-          className="bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700 transition duration-200"
-        >
-          Delete
-        </button>
+  <div className="modal-backdrop fade show d-flex justify-content-center align-items-center" style={{position:'fixed',inset:0,backgroundColor:"#4B5563BF",zIndex:1050}}>
+  <div className="modal-dialog modal-dialog-centered">
+      <div className="modal-content rounded-lg shadow-lg">
+          <div className="modal-header">
+              <h5 className="modal-title">Confirm Deletion</h5>
+          </div>
+          <div className="modal-body">
+              <p>
+                  Are you sure you want to delete <span className="text-primary font-weight-bold">{deletetopic}</span> topic?
+              </p>
+          </div>
+          <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" onClick={()=>{closeModal()}}>
+                  Cancel
+              </button>
+              <button type="button" className="btn btn-danger" onClick={()=>{deleteTopics()}}>
+                  Delete
+              </button>
+          </div>
       </div>
-    </div>
   </div>
+</div>
+
 )}
 
             {edit && 
